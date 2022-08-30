0671aa8e2e5aaf878599af416af3636eba086889
Minh Pham <54668379+pcminh0505@users.noreply.github.com>
2022-08-30 17:30:05 +0700

    [Setup] Re-structure Repository (#2)

---
+++ Sporter/MVVM/Models/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  Sporter
+//
+//  Created by Minh Pham on 30/08/2022.
+//
+
+import Foundation
---
+++ Sporter/MVVM/ViewModels/AuthenticationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationViewModel.swift
+//  Sporter
+//
+//  Created by Minh Pham on 30/08/2022.
+//
+
+import Foundation
